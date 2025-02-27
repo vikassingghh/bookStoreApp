3dac2c5a3b37efe29ea1ffca3d017b17ef0a7fb3
vikassingghh <vikasram2501@gmail.com>
2025-02-27 12:47:18 +0530

    Project complete

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
             password: data.password,
           }
           await axios
-          .post("http://localhost:4001/users/login", userInfo)
+          .post("https://bookstoreapp-backend-8y5k.onrender.com/users/login", userInfo)
           .then((res) => {
             console.log(res.data);
             if(res.data){
